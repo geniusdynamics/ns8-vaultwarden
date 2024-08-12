5b493a34a163666295f8914c07214240d1d73e76
Martin Bhuong <martin@genius.ke>
2024-08-12 17:26:37 +0200

    update vaultwarden version to 1.32.0

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="vaultwarden"
-vaultwarden_version="1.31.0"
+vaultwarden_version="1.32.0"
 
 # Create a new empty container image
 container=$(buildah from scratch)
