fb270b181091703490eeab905ee73cf3a72b8073
Kemboi Elvis <kemboielvis22@gmail.com>
2025-08-21 04:05:48 +0300

    fix: it should be 1.34.3

--- build-images.sh
+++ build-images.sh
@@ -14,7 +14,7 @@ images=()
 repobase="${REPOBASE:-ghcr.io/geniusdynamics}"
 # Configure the image name
 reponame="vaultwarden"
-vaultwarden_version="1.34.8"
+vaultwarden_version="1.34.3"
 
 # Create a new empty container image
 container=$(buildah from scratch)
